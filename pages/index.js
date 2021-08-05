dac936802b8ae491a08dec902e834923cf8253a7
shiva-nanaware <shiva.nanaware@gmail.com>
2021-08-05 14:29:01 +0530

    added lint

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,30 @@
 import useSwr from 'swr'
 import Link from 'next/link'
+import axios from "axios";
 
-const fetcher = (url) => fetch(url).then((res) => res.json())
+const fetcher = url => axios.get(url).then(res => res.data);
 
 export default function Index() {
-  const { data, error } = useSwr('/api/users', fetcher)
+  const { data, error } = useSwr('https://swapi.dev/api/planets/', fetcher)
 
   if (error) return <div>Failed to load users</div>
   if (!data) return <div>Loading...</div>
+  console.log('data:', data);
 
   return (
     <ul>
-      {data.map((user) => (
-        <li key={user.id}>
-          <Link href="/user/[id]" as={`/user/${user.id}`}>
-            <a>{`User ${user.id}`}</a>
+      {data.results.map((user) => (
+        <li key={user.name}>
+          <Link href="/user/[id]" as={`/user/${user.name}`}>
+            <a>{`${user.name}`}</a>
           </Link>
         </li>
       ))}
+      <li key={"chart"}>
+      <Link href="/user/chart" >
+            <a>{`Chart`}</a>
+          </Link>
+      </li>
     </ul>
   )
 }
