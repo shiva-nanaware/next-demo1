7223e038ee17e8f9794049f35e041e050eb7a732
Shivdas Nanaware <shiva.nanaware@gmail.com>
2021-08-12 16:29:15 +0530

    Correction in code

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,9 @@ export default function Index() {
   //const { data, error } = useSwr('/api/users', fetcher)
   //const { data, error } = useSwr('https://swapi.dev/api/planets/', fetcher)
 
-//   if (error) return <div>Failed to load users</div>
-//   if (!data) return <div>Loading...</div>
-//   console.log('data:', data);
+//    if (error) return <div>Failed to load users</div>
+//    if (!data) return <div>Loading...</div>
+//    console.log('data:', data);
 
   return (
     <ul>
@@ -22,21 +22,20 @@ export default function Index() {
 //           </Link>
 //         </li>
 //       ))}
-      {/* {data.results.map((user) => (
-        <li key={user.name}>
-          <Link href="/user/[id]" as={`/user/${user.name}`}>
-            <a>{`${user.name}`}</a>
-          </Link>
-        </li>
-      )) */}
+//        {data.results.map((user) => (
+//         <li key={user.name}>
+//           <Link href="/user/[id]" as={`/user/${user.name}`}>
+//             <a>{`${user.name}`}</a>
+//           </Link>
+//         </li>
+//       ))} 
       <li key={"chart"}>
-      <Link href="/user/chart" >
+        <Link href="/user/chart" >
             <a>{`Chart`}</a>
           </Link>
       </li>
-
       <li key={"post"}>
-      <Link href="/user/empRoute" >
+        <Link href="/user/empRoute" >
             <a>{`POST`}</a>
           </Link>
       </li>
